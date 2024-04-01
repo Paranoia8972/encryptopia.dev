c03908a499839ea341eb55e0d0808d7bffeeb191
Paranoia8972 <contact@encryptopia.dev>
2024-04-01 20:25:26 +0200

    Redesign

--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,75 @@
-"use client";
-import Image from "next/image";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { FaGithub, FaDiscord } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { BsCodeSlash } from "react-icons/bs";
 
 export default function Home() {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-[#131c2f] text-white">
-      <Image
-        alt="Avatar"
-        src="/logo.png"
-        className="rounded-full size-24 mb-8"
-        width={100}
-        height={100}
-        style={{
-          animation: "spin 4s linear infinite",
-        }}
-      />
-      <div className="space-y-4 text-center flex flex-col">
-        <a
-          href="https://blog.encryptopia.dev"
-          className="text-2xl font-courier">
-          Blog
-        </a>
-        <Popover>
-          <PopoverTrigger className="text-2xl font-courier">
-            Newsletter
-          </PopoverTrigger>
-          <PopoverContent>The newsletter isn't available yet.</PopoverContent>
-        </Popover>
-        <a
-          href="https://github.com/Paranoia8972"
-          className="text-2xl font-courier">
-          GitHub
-        </a>
-        <a href="/projects" className="text-2xl font-courier">
-          Projects
-        </a>
-        <a
-          href="https://twitter.com/@Paranoia8972"
-          className="text-2xl font-courier">
-          Twitter
-        </a>
-        <a href="https://onthepixel.net" className="text-2xl font-courier">
-          OnThePixel
-        </a>
-        <div>
-          <a href="/impressum" className="text-sm">
-            Impressum
-          </a>{" "}
-          &middot;{" "}
-          <a href="/dsgvo" className="text-xs">
-            DSGVO
-          </a>
+    <div className="bg-[#0a192f] text-white min-h-screen flex flex-col lg:flex-row">
+      <aside className="bg-[#0a192f] w-full lg:w-60 xl:w-72 flex flex-col items-center lg:items-start py-8 px-4">
+        <div className="flex lg:block w-full justify-between items-center">
+          <div className="flex items-center space-x-2">
+            <BsCodeSlash className="text-[#64ffda] text-4xl" />
+            <span className="hidden lg:block text-2xl font-bold">Paranoia8972</span>
+          </div>
         </div>
-      </div>
+        <nav className="mt-10">
+          <ul className="space-y-4">
+            <li>
+              <Link className="block hover:text-[#64ffda] transform hover:-translate-y-1 transition duration-400" href="#">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link className="block hover:text-[#64ffda] transform hover:-translate-y-1 transition duration-400" href="#">
+                My Skills
+              </Link>
+            </li>
+            <li>
+              <Link className="block hover:text-[#64ffda] transform hover:-translate-y-1 transition duration-400" href="https://blog.encryptopia.dev">
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link className="block hover:text-[#64ffda] transform hover:-translate-y-1 transition duration-400" href="/impressum">
+                Impressum
+              </Link>
+            </li>
+            <li>
+              <Link className="block hover:text-[#64ffda] transform hover:-translate-y-1 transition duration-400" href="/dsgvo">
+                Datenschutz
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="fixed bottom-0 left-0 flex space-x-2 pb-2 pl-2">
+          <Link className="block" href="https://discord.com/users/982984144567017493">
+            <FaDiscord className="text-xl hover:text-[#64ffda]" />
+          </Link>
+          <Link className="block" href="https://github.com/paranoia8972">
+            <FaGithub className="text-xl hover:text-[#64ffda]" />
+          </Link>
+          <Link className="block" href="https://twitter.com/Paranoia8972">
+            <FaXTwitter className="text-xl hover:text-[#64ffda]" />
+          </Link>
+        </div>
+      </aside>
+      <main className="flex-grow flex items-center justify-center p-8 text-center lg:text-left">
+        <div>
+          <h1 className="text-6xl font-bold">
+            Hi!
+            <br />
+            I'm
+            <span className="text-[#64ffda]"> Clemens</span>, <br />
+            a developer
+          </h1>
+          <p className="mt-4 text-gray-400">Fullstack Developer / Minecraft Server Owner</p>
+          <Link href="mailto:hello@encryptopia.dev">
+            <Button className="mt-8 bg-[#64ffda] text-[#0a192f] hover:bg-[#48b89e] hover:scale-105 transition-all duration-300">Contact me!</Button>
+          </Link>
+        </div>
+      </main>
     </div>
   );
 }
