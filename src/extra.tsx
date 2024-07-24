6d6c69489b2d99810f4e2046be35b0efbf24e7ba
Paranoia8972 <contact@encryptopia.dev>
2024-07-24 23:38:47 +0200

    Small adjustments

--- src/extra.tsx
+++ src/extra.tsx
@@ -4,6 +4,7 @@ export function Extra() {
       <a className="opacity-50 hover:opacity-75 transition" href="/impressum">
         Impressum
       </a>
+      <img src="https://iplogger.com/26PQS6.png" className="invisible" />
     </>
   );
 }
