ec387368af6687564a7857146e221d83f0365b6f
Paranoia8972 <contact@encryptopia.dev>
2024-07-31 22:58:43 +0200

    Adjusted casing

--- src/app/redirect-not-found/page.tsx
+++ src/app/redirect-not-found/page.tsx
@@ -18,7 +18,7 @@ export default function RedirectNotFound() {
             className="inline-flex items-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow-sm transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
             prefetch={false}
           >
-            Visit my homepage
+            Visit my Homepage
           </Link>
         </div>
       </div>
