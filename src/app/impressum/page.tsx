1bb16d05f8b2c90139216eb4d48d814ca9e8871f
Clemens <contact@encryptopia.dev>
2024-08-30 14:36:54 +0200

    Moved to next/navigation

--- src/app/impressum/page.tsx
+++ src/app/impressum/page.tsx
@@ -1,6 +1,6 @@
 import { FaArrowLeft } from "react-icons/fa";
 import Link from "next/link";
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function Impressum() {
   const router = useRouter();
