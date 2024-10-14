95f0beed4050cf3a52cd91cb4637ebddc0a97e28
Paranoia8972 <contact@encryptopia.dev>
2024-10-14 16:49:17 +0200

    Added rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/rss.xml",
+        destination: "/feed/rss.xml",
+      },
+      {
+        source: "/atom.xml",
+        destination: "/feed/atom.xml",
+      },
+      {
+        source: "/feed.json",
+        destination: "/feed/feed.json",
+      },
+      {
+        source: "/rss",
+        destination: "/feed/rss.xml",
+      },
+      {
+        source: "/feed",
+        destination: "/feed/rss.xml",
+      },
+      {
+        source: "/atom",
+        destination: "/feed/atom.xml",
+      },
+      {
+        source: "/json",
+        destination: "/feed/feed.json",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
