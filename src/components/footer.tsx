736397dd066b440765bf011a5a3f1b168435fa1a
Paranoia8972 <contact@encryptopia.dev>
2024-10-14 19:58:36 +0200

    improved performance and updated umami

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,12 +10,14 @@ const YEAR = new Date().getFullYear();
 interface SocialLinkProps {
   href: string;
   icon: React.ComponentType;
+  label: string;
 }
 
-function SocialLink({ href, icon: Icon }: SocialLinkProps) {
+function SocialLink({ href, icon: Icon, label }: SocialLinkProps) {
   return (
     <a href={href} target="_blank" rel="noopener noreferrer">
       <Icon />
+      <span className="sr-only">{label}</span>
     </a>
   );
 }
@@ -23,11 +25,16 @@ function SocialLink({ href, icon: Icon }: SocialLinkProps) {
 function SocialLinks() {
   return (
     <div className="flex text-lg gap-3.5 float-right transition-opacity duration-300 hover:opacity-90">
-      <SocialLink href={socialLinks.twitter} icon={FaXTwitter} />
-      <SocialLink href={socialLinks.github} icon={FaGithub} />
-      <SocialLink href={socialLinks.email} icon={TbMailFilled} />
-      <a href="/rss.xml" target="_self">
+      <SocialLink
+        href={socialLinks.twitter}
+        icon={FaXTwitter}
+        label="Twitter"
+      />
+      <SocialLink href={socialLinks.github} icon={FaGithub} label="GitHub" />
+      <SocialLink href={socialLinks.email} icon={TbMailFilled} label="Email" />
+      <a href="/rss.xml" target="_self" aria-label="RSS Feed">
         <FaRss />
+        <span className="sr-only">RSS Feed</span>
       </a>
     </div>
   );
--- src/config.ts
+++ src/config.ts
@@ -14,5 +14,5 @@ export const socialLinks = {
 
 export const umami = {
   websiteId: "405b74dd-141a-439c-9bdd-3392f560f7dd", // umami website id
-  src: "https://umami-encryptopia.app/script.js", // umami src
+  src: "https://umami-seven-topaz.vercel.app/script.js", // umami src
 };
