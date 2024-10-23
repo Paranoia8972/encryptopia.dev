fa87e8abfc815edd4978ec2a5b29468dd388ed71
Paranoia8972 <contact@encryptopia.dev>
2024-10-24 00:56:41 +0200

    updated layout

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -10,36 +10,38 @@ export default function BlogPosts() {
   const allBlogs = getBlogPosts();
 
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-medium tracking-tight">My Blog</h1>
-      <div>
-        {allBlogs
-          .sort((a, b) => {
-            if (
-              new Date(a.metadata.publishedAt) >
-              new Date(b.metadata.publishedAt)
-            ) {
-              return -1;
-            }
-            return 1;
-          })
-          .map((post) => (
-            <Link
-              key={post.slug}
-              className="flex flex-col space-y-1 mb-4 transition-opacity duration-200 hover:opacity-80"
-              href={`/blog/${post.slug}`}
-            >
-              <div className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-1 sm:space-y-0 sm:space-x-2">
-                <p className="text-black dark:text-white tracking-tight">
-                  {post.metadata.title}
-                </p>
-                <p className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
-                  {formatDate(post.metadata.publishedAt, false)}
-                </p>
-              </div>
-            </Link>
-          ))}
-      </div>
-    </section>
+    <div className="mt-16 flex min-h-screen justify-center">
+      <section className="w-full max-w-[640px] px-4">
+        <h1 className="mb-8 text-2xl font-medium tracking-tight">My Blog</h1>
+        <div>
+          {allBlogs
+            .sort((a, b) => {
+              if (
+                new Date(a.metadata.publishedAt) >
+                new Date(b.metadata.publishedAt)
+              ) {
+                return -1;
+              }
+              return 1;
+            })
+            .map((post) => (
+              <Link
+                key={post.slug}
+                className="mb-4 flex flex-col space-y-1 transition-opacity duration-200 hover:opacity-80"
+                href={`/blog/${post.slug}`}
+              >
+                <div className="flex w-full flex-col items-start justify-between space-y-1 sm:flex-row sm:items-center sm:space-x-2 sm:space-y-0">
+                  <p className="tracking-tight text-black dark:text-white">
+                    {post.metadata.title}
+                  </p>
+                  <p className="text-sm tabular-nums text-neutral-600 dark:text-neutral-400">
+                    {formatDate(post.metadata.publishedAt, false)}
+                  </p>
+                </div>
+              </Link>
+            ))}
+        </div>
+      </section>
+    </div>
   );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -9,33 +9,35 @@ export const metadata: Metadata = {
 
 export default function Projects() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-medium tracking-tight">Projects</h1>
-      <div className="space-y-6">
-        {projects.map((project, index) => (
-          <a
-            key={index}
-            href={project.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="block group hover:opacity-80 transition-opacity duration-200"
-          >
-            <div className="flex flex-col">
-              <div className="w-full flex justify-between items-baseline">
-                <span className="text-black dark:text-white font-medium tracking-tight">
-                  {project.title}
-                </span>
-                <span className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
-                  {project.year}
-                </span>
+    <div className="mt-16 flex min-h-screen justify-center">
+      <section className="w-full max-w-[640px] px-4">
+        <h1 className="mb-8 text-2xl font-medium tracking-tight">Projects</h1>
+        <div className="space-y-6">
+          {projects.map((project, index) => (
+            <a
+              key={index}
+              href={project.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group block transition-opacity duration-200 hover:opacity-80"
+            >
+              <div className="flex flex-col">
+                <div className="flex w-full items-baseline justify-between">
+                  <span className="font-medium tracking-tight text-black dark:text-white">
+                    {project.title}
+                  </span>
+                  <span className="text-sm tabular-nums text-neutral-600 dark:text-neutral-400">
+                    {project.year}
+                  </span>
+                </div>
+                <p className="prose prose-neutral pt-3 dark:prose-invert">
+                  {project.description}
+                </p>
               </div>
-              <p className="prose prose-neutral dark:prose-invert pt-3">
-                {project.description}
-              </p>
-            </div>
-          </a>
-        ))}
-      </div>
-    </section>
+            </a>
+          ))}
+        </div>
+      </section>
+    </div>
   );
 }
