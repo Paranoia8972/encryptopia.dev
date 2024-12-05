9266762ac5fdab4f53b256b2b142e54f2e84afc0
Paranoia8972 <contact@encryptopia.dev>
2024-12-05 17:22:11 +0100

    refactor: simplify page structure by replacing div with article element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Link from "next/link";
-
 export default function Page() {
   return (
-    <main className="prose prose-invert max-w-none flex-grow">
-      <div className="prose prose-neutral dark:prose-invert">
+    <main className="flex-grow">
+      <article className="prose prose-neutral dark:prose-invert">
         <p>
           I&apos;m a 15 year old high school student in 10th grade. I love
           exploring and working with Servers (Minecraft, Linux), Web Development
@@ -13,7 +11,7 @@ export default function Page() {
           I enjoy self-hosting stuff, enhancing my ethical hacking skills, and
           contributing to open-source projects.
         </p>
-      </div>
+      </article>
     </main>
   );
 }
