42be63b699846b701cfd87e891a57dd03d6360a1
Paranoia8972 <contact@encryptopia.dev>
2024-12-05 20:18:03 +0100

    refactor: integrate SpeedInsights component into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
+        <SpeedInsights />
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
