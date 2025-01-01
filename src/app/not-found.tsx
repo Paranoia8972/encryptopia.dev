cc370586e57443055a55f85077952cd0ed4fe35a
Paranoia8972 <contact@encryptopia.dev>
2025-01-02 00:13:28 +0100

    fix: update 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,13 +2,13 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "404",
-  description: "Error 404",
+  description: "404 - Page Not Found",
 };
 
 export default function NotFound() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-medium tracking-tight">
+      <h1 className="mb-8 text-2xl font-medium tracking-tight" id="toc-ignore">
         404 - Page not found
       </h1>
       <p className="mb-4">
